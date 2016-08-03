e0dddd0f56d19526e33938a2e26665a2d8368982
Bartol Karuza <bartol@courgetteconcepts.com>
2016-08-03 18:50:02 +0200

    permutations of any length for all possible variable combinations

--- packager/react-packager/src/node-haste/lib/permutations.js
+++ packager/react-packager/src/node-haste/lib/permutations.js
@@ -20,9 +20,7 @@ function permutations(options) {
     let cur;
     for (let i = 0; i < arr.length; ++i) {
       cur = arr.splice(i, 1);
-      if (arr.length === 0) {
-        results.push(memo.concat(cur));
-      }
+      results.push(memo.concat(cur));
       permute(arr.slice(), memo.concat(cur));
       arr.splice(i, 0, cur[0]);
     }
